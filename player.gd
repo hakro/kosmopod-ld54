5fe038c77999b7b9956844190a8af77d46fdbc68
Hakim <hakim.rouatbi@gmail.com>
2023-10-01 00:03:40 +0200

    Turn around & Basic win area

--- player.gd
+++ player.gd
@@ -6,36 +6,39 @@ var rotating : bool = false
 @onready var move_raycast : RayCast3D = $MoveRayCast3D
 # Raycast to get the object in front of the player
 @onready var obj_raycast : RayCast3D = $ObjectRayCast3D
+# Raycast to detect win area
+@onready var win_raycast : RayCast3D = $WinRayCast3D
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("move_forward"):
 		move(Vector3.FORWARD)
+	if Input.is_action_just_pressed("turn_around"):
+		turn(PI)
 	if Input.is_action_just_pressed("turn_left"):
 		turn(PI/2)
 	if Input.is_action_just_pressed("turn_right"):
 		turn(-PI/2)
-	
+
 	# Attract Objects
 	if Input.is_action_just_pressed("pull_object"):
 		#print(obj_raycast.get_collision_normal())
 		if obj_raycast.is_colliding():
 			var obj = obj_raycast.get_collider()
-			var offset = position - obj.position
-			#obj.position += offset - obj_raycast.get_collision_normal()
-			obj.move_by(offset - obj_raycast.get_collision_normal())
-		
+			if obj.is_in_group("movable"):
+				var offset = position - obj.position
+				#obj.position += offset - obj_raycast.get_collision_normal()
+				obj.move_by(offset - obj_raycast.get_collision_normal())
 
 func move(dir: Vector3):
-	#var rot_tween : Tween = create_tween()
-	#rot_tween.tween_property(self, "rotation:y", Vector3.FORWARD.signed_angle_to(dir, Vector3.UP), 0.05)
-	#await rot_tween.finished
-	#move_raycast.force_raycast_update()
 	if not moving and not rotating and not move_raycast.is_colliding():
 		moving = true
 		var move_tween : Tween = create_tween()
 		move_tween.tween_property(self, "position", dir.rotated(Vector3.UP, rotation.y), 0.2).as_relative().set_trans(Tween.TRANS_CUBIC)
 		await move_tween.finished
 		moving = false
+
+	if win_raycast.is_colliding():
+		print("win")
 
 func turn(angle : float):
 	if not moving and not rotating:
